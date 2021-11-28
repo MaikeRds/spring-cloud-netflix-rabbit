706ca3792cf4d802ddb56a6c8234a2f48661af2c
Maike Rodrigues <maikerodrigues@cialne.com.br>
2021-11-27 22:57:16 -0300

    implementa o service discovery

--- auth/src/main/java/com/example/auth/AuthApplication.java
+++ auth/src/main/java/com/example/auth/AuthApplication.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -13,6 +14,7 @@ import com.example.auth.entities.User;
 import com.example.auth.repositories.PermissionRepository;
 import com.example.auth.repositories.UserRepository;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthApplication {
 
